ce5b02816e360d883948785f7594d253b4ccfc15
Kakon Ray <thisiskakonray@gmail.com>
2022-05-04 00:25:01 +0600

    access control allow cors

--- index.js
+++ index.js
@@ -137,6 +137,11 @@ app.listen(port, () => {
   console.log(`CROUD server is Running ${port}`);
 });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+// CORS Error solve
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
