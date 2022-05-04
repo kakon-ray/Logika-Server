56121e6bd094705038c5d8d4adbd6af2ff8a101c
Kakon Ray <thisiskakonray@gmail.com>
2022-05-04 13:34:31 +0600

    cors polici

--- index.js
+++ index.js
@@ -174,9 +174,9 @@ app.listen(port, () => {
 
 // CORS Error solve
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "application/json");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "application/json");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
